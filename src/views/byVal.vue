79685430418aea1ea80640c8a36c6be1ebd755f7
shanxinlin <36286636+shanxinlin@users.noreply.github.com>
2021-03-29 18:07:05 +0800

    Update byVal.vue

--- src/views/byVal.vue
+++ src/views/byVal.vue
@@ -2,10 +2,11 @@
   <div>
     <h3>传值的方式</h3>
     <br />
-    <br />
-    <br />
     <div>{{ Name }}</div>
     <Acom v-model="Name"></Acom>
+    <button @click="fangdou()">防抖</button>
+    <button @click="fangdou2()">防抖</button>
+    <button @click="jieliu()">节流</button>
   </div>
 </template>
 
@@ -13,28 +14,50 @@
 import Acom from '@/components/byValcom/A.vue'
 export default {
   name: "byVal",
-  components:{
-      Acom
+  components: {
+    Acom
+  },
+  provide: {
+    kuaji: 'demo'
   },
   data() {
     return {
       Name: 1,
+      time: null,
+      timmer: null,
+      JLTime: false,
+      last: 0
     };
   },
-  beforeCreate() {
-    console.log("父：beforeCreate");
-  },
-  created() {
-    console.log("父：created");
-  },
-  beforeMount() {
-    console.log("父：beforeMount");
-  },
-  mounted() {
-    console.log("父：mounted");
+  mounted(){
+    console.log('获取子组件数据', this.$children)
   },
   methods: {
-    namFn() {},
+    fangdou() {
+      this.time && clearTimeout(this.time);
+      this.time = setTimeout(() => {
+        console.log('防抖')
+      }, 1000)
+    },
+    fangdou2() {
+      this.JLTime = !this.timmer
+      if (this.timmer) {
+        clearTimeout(this.timmer)
+      }
+      this.timmer = setTimeout(() => {
+        this.timmer = null
+      }, 1000)
+      if (this.JLTime) {
+        console.log('防抖2')
+      }
+    },
+    jieliu() {
+      var now = Date.now()
+      if (now - this.last > 1000) {
+        console.log('节流')
+        this.last = now
+      }
+    }
   },
 };
 </script>
