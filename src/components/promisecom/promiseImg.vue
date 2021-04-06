7f9a0a50beff364a2ef5190279edd9bf8ccc1f56
shanxinlin <36286636+shanxinlin@users.noreply.github.com>
2021-04-06 17:52:48 +0800

    Update promiseImg.vue

--- src/components/promisecom/promiseImg.vue
+++ src/components/promisecom/promiseImg.vue
@@ -1,42 +1,35 @@
 <template>
-    <div>
-        <div>
-            promise图片预加载
-        </div>
-        <div>
-
-        </div>
-    </div>
+  <div>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            arr: []
-        }
-    },
-    mounted(){
-        for(let i = 0; i < 3; i++){
-            this.arr.push(this.loadImg(`img-${i}.jpg`,i))
-        }
-        Promise.race(this.arr).then((imgarr)=>{
-            console.log(imgarr)
-        })
-    },
-    methods:{
-        loadImg(_src, index){
-            return new Promise((resolve, reject)=>{
-                let img = new Image();
-                img.src = _src;
-                img.onload = () =>{
-                    resolve(img);
-                };
-                img.onerror = () =>{
-                    reject(`加载失败${index}`,)
-                }
-            })
-        }
+  data() {
+    return {
+      arr: []
     }
+  },
+  mounted() {
+    let p1 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('success')
+      }, 1000)
+    })
+
+    let p2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject('failed')
+      }, 500)
+    })
+
+    Promise.race([p1, p2]).then((result) => {
+      console.log(result)
+    }).catch((error) => {
+      console.log(error)  // 打开的是 'failed'
+    })
+  },
+  methods: {
+  }
 }
 </script>
