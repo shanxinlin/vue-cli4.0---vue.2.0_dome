4f7685fd12fe62892e9cc192c02ad6f7387be684
shanxinlin <36286636+shanxinlin@users.noreply.github.com>
2021-04-06 18:01:27 +0800

    Update promiseText.vue

--- src/components/promisecom/promiseText.vue
+++ src/components/promisecom/promiseText.vue
@@ -1,1 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+      arr: []
+    }
+  },
+  mounted() {
+    
+  },
+  methods: {
+  }
+}
+</script>
