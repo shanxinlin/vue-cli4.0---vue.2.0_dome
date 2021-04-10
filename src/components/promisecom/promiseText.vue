3d1837e55f021b9ec421a8fff11b0d39e4c3f654
shanxinlin <36286636+shanxinlin@users.noreply.github.com>
2021-04-10 14:24:37 +0800

    Update promiseText.vue

--- src/components/promisecom/promiseText.vue
+++ src/components/promisecom/promiseText.vue
@@ -11,7 +11,36 @@ export default {
     }
   },
   mounted() {
-    
+    let FN = new Promise(resolve => {
+      setTimeout(function() {
+        console.log('2:   ')
+        resolve(2);
+      }, 1000);
+    });
+    FN.then((res) => {
+      console.log('4:   ', res)
+    })
+
+    function Promise(fn) {
+      console.log('0:    ', fn)
+      var value = null,
+        callbacks = [];  //callbacks为数组，因为可能同时有很多个回调
+      this.then = function(onFulfilled) {
+        console.log('1:   ', onFulfilled)
+        callbacks.push(onFulfilled);
+      };
+      function resolve(value) {
+        console.log('3:   ', value)
+        callbacks.forEach(function(callback) {
+          callback(value);
+        });
+        console.log('5:', value)
+      }
+      setTimeout(function() {
+        console.log('2:   ')
+        resolve(2);
+      }, 1000);
+    }
   },
   methods: {
   }
