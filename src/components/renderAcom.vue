26c1a1fc75b8619b6872861679421df7e34c722b
shanxinlin <36286636+shanxinlin@users.noreply.github.com>
2021-04-16 11:14:44 +0800

    Create renderAcom.vue

---
+++ src/components/renderAcom.vue
@@ -0,0 +1,23 @@
+
+<script>
+export default {
+  props: {
+    tag: String
+  },
+  data() {
+    return {
+      people: ['clearlove', 'viper', 'meikou']
+    }
+  },
+  render(createElement) {
+    return createElement(this.tag, {}, this.people.map(name => createElement('li', {
+      attrs: { class: 'text' },
+      on: {
+        click: () => {
+          console.log(name)
+        }
+      }
+    }, `非组件：${name}`)))
+  }
+}
+</script>
