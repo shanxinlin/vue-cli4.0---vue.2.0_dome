74cf9bde4e210d84cbf673ff90bbed69f8703c47
shanxinlin <36286636+shanxinlin@users.noreply.github.com>
2021-04-16 11:15:42 +0800

    Create render.vue

---
+++ src/views/render.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>什么是render</div>
+    <renderAcom tag="ul" />
+    <renderBcom></renderBcom>
+  </div>
+</template>
+<script>
+import renderAcom from '@/components/renderAcom.vue'
+import renderBcom from '@/components/renderBcom.vue'
+export default {
+  components: {
+    renderAcom,
+    renderBcom
+  }
+}
+</script>
