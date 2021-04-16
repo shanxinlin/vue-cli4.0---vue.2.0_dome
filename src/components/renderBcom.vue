08acb23bca1649efae2fa72860790067d9f4a90a
shanxinlin <36286636+shanxinlin@users.noreply.github.com>
2021-04-16 11:14:59 +0800

    Create renderBcom.vue

---
+++ src/components/renderBcom.vue
@@ -0,0 +1,13 @@
+
+<script>
+import renderText from '@/components/renderText.vue'
+export default {
+  render(createElement) {
+    return createElement(renderText, {
+      props: {
+        msg: '哈默'
+      }
+    })
+  }
+}
+</script>
