5adb1eede660e5654f22b699546f2f17eaa2ee37
shanxinlin <36286636+shanxinlin@users.noreply.github.com>
2021-05-12 10:39:57 +0800

    Update loading.vue

--- src/assets/js/common/loading/loading.vue
+++ src/assets/js/common/loading/loading.vue
@@ -17,11 +17,21 @@ export default {
 </script>
 <style lang="less" scoped>
 .loading-wrap {
-  width: 100%;
-  height: 100%;
-  background: #eee;
-  padding: 20px;
+  position: absolute;
+  z-index: 2000;
+  background-color: hsla(0, 0%, 100%, 0.9);
+  margin: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transition: opacity 0.3s;
 }
 .loading-box {
+  top: 50%;
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  transform: translateY(-50%);
 }
 </style>
